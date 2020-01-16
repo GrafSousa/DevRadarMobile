b3a1f4138e366c487e1ae8f9d7776ba522e55dad
Gustavo <graf.sousa@gmail.com>
2020-01-16 15:50:14 -0300

    Adiciona tela Main

---
+++ src/pages/Main.js
@@ -0,0 +1,80 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main() {
+  const [currentRegion, setCurrentRegion] = useState(null);
+
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const { granted } = await requestPermissionsAsync();
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true,
+        });
+        const { latitude, longitude } = coords;
+
+        setCurrentRegion({
+          latitude,
+          longitude,
+          latitudeDelta: 0.04,
+          longitudeDelta: 0.04,
+        });
+      }
+    }
+
+    loadInitialPosition();
+  }, []);
+
+  if (!currentRegion) {
+    return null;
+  }
+
+  return (
+    <MapView initialRegion={currentRegion} style={styles.map}>
+      <Marker coordinate={{ latitude: -19.921356, longitude: -43.93414 }}>
+        <Image
+          style={styles.avatar}
+          source={{ uri: 'https://avatars3.githubusercontent.com/u/17460444?s=460&v=4' }}
+        />
+        <Callout>
+          <View style={styles.callout}>
+            <Text style ={styles.devName}>Gustavo Graf de Sousa</Text>
+            <Text style ={styles.devBio}>Bio</Text>
+            <Text style ={styles.devTechs}>ReactJs, React Native, Node.js</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
+  );
+}
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1
+  },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius: 4,
+    borderWidth: 4,
+    borderColor: '#FFF',
+  },
+  callout: {
+    width: 260,
+  },
+  devName: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  devBio: {
+    color: '#666',
+    marginTop: 6,
+  },
+  devTechs:{
+    marginTop: 5
+  }
+});
+
+export { Main };
